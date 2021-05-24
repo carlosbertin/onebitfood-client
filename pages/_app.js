167024d0a53e0599f96b41e33028ea0f3aa68fae
Carlos Bertin <carlosbertin@id.uff.br>
2021-05-24 13:16:25 -0300

    por Carlos Bertin: criando o front-end do onebitfood em next.js

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import '../styles/globals.scss'
+import Header from '../components/Header';
+import Container from 'react-bootstrap/Container';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>OneBitFood V2</title>
+        <link ref='icon' href='/favicon.icon' />
+      </Head>
+
+      <main>
+        <Header />
+          <Container className='mt-5'>
+            <Component {...pageProps} />
+          </Container>
+      </main>
+    </>
+  )
+}
+
+export default MyApp
